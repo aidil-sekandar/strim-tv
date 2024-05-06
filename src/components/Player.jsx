23279c40ba12a48c20df6509a3b27871aba74c7b
aidil-sekandar <aidiliskandar63@gmail.com>
2024-05-07 04:07:06 +0800

    add player component using react for android support

---
+++ src/components/Player.jsx
@@ -0,0 +1,22 @@
+import { useRef, useEffect } from "react";
+import Hls from "hls.js";
+
+function HLSPlayer({ url }) {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(url);
+      hls.attachMedia(video);
+    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+      video.src = url;
+    }
+  }, [url]);
+
+  return <video ref={videoRef} controls />;
+}
+
+export default HLSPlayer;
