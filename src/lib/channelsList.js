bc803b5eb21b0917f5f99705a173432da211272a
aidil-sekandar <aidiliskandar63@gmail.com>
2024-05-06 09:39:39 +0800

    Add channels, individual page, and logo

---
+++ src/lib/channelsList.js
@@ -0,0 +1,105 @@
+export const channelsList = [
+  {
+    name: "TV1",
+    source:
+      "https://raw.githubusercontent.com/MIFNtechnology/siaranMy/main/channels/Tv1/index.m3u8",
+    slug: "tv1",
+  },
+  {
+    name: "TV2",
+    source:
+      "https://raw.githubusercontent.com/MIFNtechnology/siaranMy/main/channels/Tv2/index.m3u8",
+    slug: "tv2",
+  },
+  {
+    name: "TV3",
+    source:
+      "https://raw.githubusercontent.com/MIFNtechnology/siaranMy/main/channels/Tv3/index.m3u8",
+    slug: "tv3",
+  },
+  {
+    name: "TV9",
+    source:
+      "https://raw.githubusercontent.com/MIFNtechnology/siaranMy/main/channels/Tv9/index.m3u8",
+    slug: "tv9",
+  },
+  {
+    name: "TVS",
+    source:
+      "https://raw.githubusercontent.com/MIFNtechnology/siaranMy/main/channels/Tvs/index.m3u8",
+    slug: "tvs",
+  },
+  {
+    name: "TV6",
+    source:
+      "https://raw.githubusercontent.com/MIFNtechnology/siaranMy/main/channels/Tv6/index.m3u8",
+    slug: "tv6",
+  },
+  {
+    name: "Cartoon Network (SEA)",
+    source: "https://cdn1.skygo.mn/live/disk1/Cartoon_Network/HLS-FTA/Cartoon_Network.m3u8",
+    slug: "cartoon-network-sea",
+  },
+  {
+    name: "Astro Awani",
+    source: "https://raw.githubusercontent.com/aidil-sekandar/free-tv-malaysia/main/awani.m3u8",
+    slug: "astro-awani",
+  },
+  {
+    name: "DidikTV",
+    source:
+      "https://raw.githubusercontent.com/MIFNtechnology/siaranMy/main/channels/DidikTvKPM/index.m3u8",
+    slug: "didiktv",
+  },
+  {
+    name: "TV Ikim",
+    source:
+      "https://raw.githubusercontent.com/MIFNtechnology/siaranMy/main/channels/TvIkim/index.m3u8",
+    slug: "tvikim",
+  },
+  {
+    name: "Berita RTM",
+    source:
+      "https://raw.githubusercontent.com/MIFNtechnology/siaranMy/main/channels/BeritaRTM/index.m3u8",
+    slug: "berita-rtm",
+  },
+  {
+    name: "Sukan RTM",
+    source:
+      "https://raw.githubusercontent.com/MIFNtechnology/siaranMy/main/channels/SukanRTM/index.m3u8",
+    slug: "sukan-rtm",
+  },
+  {
+    name: "KBS World (ENG Sub)",
+    source: "https://kbsworld-ott.akamaized.net/hls/live/2002341/kbsworld/master.m3u8",
+    slug: "kbs-world",
+  },
+  {
+    name: "CNN (USA)",
+    source:
+      "https://turnerlive.warnermediacdn.com/hls/live/586495/cnngo/cnn_slate/VIDEO_0_3564000.m3u8",
+    slug: "cnn-usa",
+  },
+  {
+    name: "BBC (NA)",
+    source: "https://d2vnbkvjbims7j.cloudfront.net/containerA/LTN/playlist.m3u8",
+    slug: "bbc-na",
+  },
+  {
+    name: "Al Jazeera (ENG)",
+    source:
+      "https://d1cy85syyhvqz5.cloudfront.net/v1/master/7b67fbda7ab859400a821e9aa0deda20ab7ca3d2/aljazeeraLive/AJE/index.m3u8",
+    slug: "aljazeera",
+  },
+  {
+    name: "TvN Movies (Indo Sub)",
+    source: "http://210.210.155.37/dr9445/h/h21/index.m3u8",
+    slug: "tvn-movies-sub-indo",
+  },
+  {
+    name: "8TV",
+    source:
+      "https://live-streams-ssai-01.tonton.com.my/live/a884c33b-6b11-4433-8bf9-a8899939e224/cmaf.isml/.m3u8",
+    slug: "8TV",
+  },
+];
